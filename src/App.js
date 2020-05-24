99ad091f6329ebb12ff810c90011a070a1dfee60
Python151 <dogm646@gmail.com>
2020-05-23 23:00:56 -0500

    added redirect fix

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ export default function App() {
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/logout" component={Logout} />
 
-          <Route path="/redirect" component={Reidrect} />
+          <Route path="/redirect" component={redirect} />
 
           <Route component={Error404} />
         </Switch>
