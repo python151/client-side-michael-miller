d6243f839572373a9525cb4f3635577e054c79c1
Python151 <dogm646@gmail.com>
2020-05-25 20:40:57 -0500

    changed redirect location

--- src/pages/Redirect.js
+++ src/pages/Redirect.js
@@ -3,11 +3,9 @@ import React from 'react'
 export default class NewestPosts extends React.Component {
     constructor(props) {
       super(props);
-      window.location = 'https://iplogger.org/2pH5v5'
+      window.location = 'michaelmiller.pythonanywhere.com/admin/'
     }
    
-
-
      render() {
         return (<h1>loading...</h1>)
     }
